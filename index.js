b9949f5c4e07d8d721dd86ea69ad21e56f038f39
Shaked Lanciano <88950077+MrsHorrid@users.noreply.github.com>
2022-11-28 09:48:11 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+const sorryBtn = document.getElementById("sorry-btn");
+const resetBtn = document.getElementById("reset-btn");
+const sorryCount = document.getElementById("sorry-count");
+const nameBad = document.getElementById("input-bad");
+const nameEnter = document.getElementById("name-enter");
+const badpName = document.getElementById("bad-p-name");
+let meme = document.getElementById("meme");
+let nameP = ''
+
+meme.style.display = `none`
+
+nameEnter.addEventListener("click", function(){
+    nameP = nameBad.value
+    nameBad.placeholder = `the name ${nameP} has been entered`
+    nameBad.value = ''
+    
+})
+
+nameEnter.placeholder = `the name${nameP} has been entered`
+
+let sorry = 0
+let hasReached = false
+
+sorryBtn.addEventListener("click", function(){
+    
+    if (sorry === 20)
+    {
+        hasReached = true
+        sorry = 20
+        badpName.innerHTML = ` ${nameP} Has been very bad by saying sorry`
+        meme.style.display = 'block'
+    }
+    else
+    {
+        sorry += 1;
+        sorryCount.textContent = sorry
+    }
+})
+
+
+resetBtn.addEventListener("click", function(){
+    sorry = 0;
+    sorryCount.textContent = sorry
+})
